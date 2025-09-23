f2c2987afe3472a650aefec2928dc090d8d91f32
Rana Muhammad Areeb <115886881+RanaAreeb@users.noreply.github.com>
2025-09-23 17:15:44 +0500

    added

--- components/footer.tsx
+++ components/footer.tsx
@@ -9,7 +9,7 @@ export function Footer() {
           {/* Brand */}
           <div className="lg:col-span-2">
             <Image
-              src="/images/emmafab-logo.png"
+              src="/images/emmafab-logo-hd.png"
               alt="EmmaFab Cosmetics"
               width={150}
               height={75}
